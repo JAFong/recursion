af153bda6d1f659738877f7744b76321a12a3209
Justin Fong <justinf715@gmail.com>
2015-04-16 18:26:10 -0700

    Complete stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // NULL
+  if (obj === null) {
+  	return obj + "";
+  }
+  // ARRAY
+  if (Array.isArray(obj)) {
+  	var result = [];
+  	for (var i = 0; i < obj.length; i++) {
+  		result.push(stringifyJSON(obj[i]));
+  	}
+  	return '[' + result + ']';
+  }
+  // OBJECT
+  if (!Array.isArray(obj) && typeof(obj)==='object') {
+  	var result = {};
+  	var output = "";
+  	  if (Object.getOwnPropertyNames(obj).length === 0){
+        return '{}';
+	  } else {
+	  	for (var prop in obj) {
+	  		if (typeof(obj[prop]) === 'function') {
+              return '{}';
+	  		} else {
+	  		  output += stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]);
+	  		  output += ",";
+	  		}
+	  	}
+	  }
+	  return "{" + output.substring(0, output.length - 1) + "}";
+  }
+  // STRING
+  if (typeof(obj) === "string") {
+  	return '"' + obj + '"';
+  }
+  // NUMBERS AND BOOLEANS
+  if (!Array.isArray(obj)) {
+  	return obj + "";
+  }
 };
